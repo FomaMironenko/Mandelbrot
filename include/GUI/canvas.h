6eabd4ad916db623d996807429cd0401d544bf24
Foma Mironenko <fmironenko@reflexion.com>
2022-06-03 01:54:00 +0300

    (REF): reorganize keyboard interactions

--- include/GUI/canvas.h
+++ include/GUI/canvas.h
@@ -13,34 +13,26 @@ const unsigned char EN_KEY_EXIT = 35; // '#'
 
 const unsigned char EN_KEY_UP_LOWCASE = 119; // 'w'
 const unsigned char EN_KEY_UP_UPPCASE = 87;
-const unsigned char RU_KEY_UP_LOWCASE = 119;
-const unsigned char RU_KEY_UP_UPPCASE = 87;
 
-const unsigned char EN_KEY_RIGHT_LOWCASE = 100;
+const unsigned char EN_KEY_RIGHT_LOWCASE = 100; // 'd'
 const unsigned char EN_KEY_RIGHT_UPPCASE = 68;
-const unsigned char RU_KEY_RIGHT_LOWCASE = 100;
-const unsigned char RU_KEY_RIGHT_UPPCASE = 68;
 
-const unsigned char EN_KEY_DOWN_LOWCASE = 115;
+const unsigned char EN_KEY_DOWN_LOWCASE = 115; // 's'
 const unsigned char EN_KEY_DOWN_UPPCASE = 83;
-const unsigned char RU_KEY_DOWN_LOWCASE = 115;
-const unsigned char RU_KEY_DOWN_UPPCASE = 83;
 
-const unsigned char EN_KEY_LEFT_LOWCASE = 97;
+const unsigned char EN_KEY_LEFT_LOWCASE = 97; // 'a'
 const unsigned char EN_KEY_LEFT_UPPCASE = 65;
-const unsigned char RU_KEY_LEFT_LOWCASE = 97;
-const unsigned char RU_KEY_LEFT_UPPCASE = 65;
 
-const unsigned char EN_KEY_PLUS = 43;
-const unsigned char EN_KEY_MINUS = 95;
+const unsigned char EN_KEY_PLUS = 43;  // '+'
+const unsigned char EN_KEY_MINUS = 95; // '-'
 
-const unsigned char EN_KEY_SPACE = 32;
+const unsigned char EN_KEY_SPACE = 32; // ' '
 
+const unsigned char EN_KEY_COLORMAP_LOWCASE = 99; // 'c'
 const unsigned char EN_KEY_COLORMAP_UPPCASE = 67;
-const unsigned char EN_KEY_COLORMAP_LOWCASE = 99;
 
+const unsigned char EN_KEY_SCREENSHOT_LOWCASE = 116; // 't'
 const unsigned char EN_KEY_SCREENSHOT_UPPCASE = 84;
-const unsigned char EN_KEY_SCREENSHOT_LOWCASE = 116;
 
 
 constexpr int size_x = 720;
